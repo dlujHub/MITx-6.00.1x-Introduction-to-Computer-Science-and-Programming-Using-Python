433e4cca97de286e6e1ae8bf8d77ebd599ce3bb1
dlujHub <dlujanschi2@gmail.com>
2018-04-02 10:59:48 +0300

    Week 2 - odd

---
+++ Week_2-Simple_Programs/4.-Functions/Exercise_odd.py
@@ -0,0 +1,20 @@
+# Exercise: odd
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 3 minutes
+
+# Write a Python function, odd, that takes in one number and returns True when the number is odd and False otherwise.
+
+# You should use the % (mod) operator, not if.
+
+# This function takes in one number and returns a boolean.
+
+
+def odd(x):
+    '''
+    x: int
+
+    returns: True if x is odd, False otherwise
+    '''
+    return x % 2 != 0
+# Correct
