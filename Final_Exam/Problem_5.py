5855072ed3e5064f0a4005b24c2ea47239c8bf74
dlujHub <dlujanschi2@gmail.com>
2018-04-20 20:34:53 +0300

    Problem 5

--- Final_Exam/Problem_5.py
+++ Final_Exam/Problem_5.py
@@ -1,98 +1,67 @@
 # Problem 5
-# 15.0/15.0 points (graded)
-# In this problem, you will implement a class according to the specifications in the template file usresident.py. The file contains a
-# Person class similar to what you have seen in lecture and a USResident class (a subclass of Person). Person is already implemented for
-# you and you will have to implement two methods of USResident.
+# 20.0/20.0 points (graded)
+#
+# Assume you are given two dictionaries d1 and d2, each with integer keys and integer values. You are also given a function f, that takes in two integers, performs an unknown operation on them, and returns a value.
+#
+# Write a function called dict_interdiff that takes in two dictionaries (d1 and d2). The function will return a tuple of two dictionaries: a dictionary of the intersect of d1 and d2 and a dictionary of the difference of d1 and d2, calculated as follows:
+#
+# intersect: The keys to the intersect dictionary are keys that are common in both d1 and d2. To get the values of the intersect dictionary, look at the common keys in d1 and d2 and apply the function f to these keys' values -- the value of the common key in d1 is the first parameter to the function and the value of the common key in d2 is the second parameter to the function. Do not implement f inside your dict_interdiff code -- assume it is defined outside.
+# difference: a key-value pair in the difference dictionary is (a) every key-value pair in d1 whose key appears only in d1 and not in d2 and (b) every key-value pair in d2 whose key appears only in d2 and not in d1.
+#
+# Here are two examples:
+#
+# If f(a, b) returns a + b
+# d1 = {1:30, 2:20, 3:30, 5:80}
+# d2 = {1:40, 2:50, 3:60, 4:70, 6:90}
+# then dict_interdiff(d1, d2) returns ({1: 70, 2: 70, 3: 90}, {4: 70, 5: 80, 6: 90})
+# If f(a, b) returns a > b
+# d1 = {1:30, 2:20, 3:30}
+# d2 = {1:40, 2:50, 3:60}
+# then dict_interdiff(d1, d2) returns ({1: False, 2: False, 3: False}, {})
+#
+# def dict_interdiff(d1, d2):
+#     '''
+#     d1, d2: dicts whose keys and values are integers
+#     Returns a tuple of dictionaries according to the instructions above
+#     '''
+#     # Your code here
+#
+# Paste your entire function, including the definition, in the box below. The function f will be automatically added to your code, do not paste it in the box. Do not leave any debugging print statements. Note that we ask you to write a function only -- you cannot rely on any variables defined outside your function for your code to work correctly.
 
-# For example, the following code:
-# a = USResident('Tim Beaver', 'citizen')
-# print(a.getStatus())
-# b = USResident('Tim Horton', 'non-resident')
 
-# will print out:
-# citizen
-# ## will show that a ValueError was raised at a particular line
-
-## DO NOT MODIFY THE IMPLEMENTATION OF THE Person CLASS ##
-class Person(object):
-    def __init__(self, name):
-        #create a person with name name
-        self.name = name
-        try:
-            firstBlank = name.rindex(' ')
-            self.lastName = name[firstBlank+1:]
-        except:
-            self.lastName = name
-        self.age = None
-    def getLastName(self):
-        #return self's last name
-        return self.lastName
-    def setAge(self, age):
-        #assumes age is an int greater than 0
-        #sets self's age to age (in years)
-        self.age = age
-    def getAge(self):
-        #assumes that self's age has been set
-        #returns self's current age in years
-        if self.age == None:
-            raise ValueError
-        return self.age
-    def __lt__(self, other):
-        #return True if self's name is lexicographically less
-        #than other's name, and False otherwise
-        if self.lastName == other.lastName:
-            return self.name < other.name
-        return self.lastName < other.lastName
-    def __str__(self):
-        #return self's name
-        return self.name
-        
-class USResident(Person):
-    """ 
-    A Person who resides in the US.
+def dict_interdiff(d1, d2):
     """
-    def __init__(self, name, status):
-        """ 
-        Initializes a Person object. A USResident object inherits 
-        from Person and has one additional attribute:
-        status: a string, one of "citizen", "legal_resident", "illegal_resident"
-        Raises a ValueError if status is not one of those 3 strings
-        """
-        # Write your code here
-        
-    def getStatus(self):
-        """
-        Returns the status
-        """
-        # Write your code here
-        
-# For this question, you will not be able to see the test cases we run. This problem will test your ability to come up with your own
-# test cases.
-
-# Paste here
-class USResident(Person):
-    """ 
-    A Person who resides in the US.
+    d1, d2: dicts whose keys and values are integers
+    Returns a tuple of dictionaries according to the instructions above
     """
-    def __init__(self, name, status):
-        """ 
-        Initializes a Person object. A USResident object inherits 
-        from Person and has one additional attribute:
-        status: a string, one of "citizen", "legal_resident", "illegal_resident"
-        Raises a ValueError if status is not one of those 3 strings
-        """
-        # Write your code here
-        super().__init__(name)
-        if status == 'citizen' or status == 'legal_resident' or status == 'illegal_resident':
-            self.status = status
+    def ff(a,b):
+        return a + b
+    def f(a,b):
+        return a > b
+    result = ({}, {})
+    for key_a, value_a in d1.items():
+        if key_a in d2.keys():
+            result[0].setdefault(key_a, f(value_a, d2.get(key_a)))
+            d2.pop(key_a)
         else:
-            raise ValueError
-        
-    def getStatus(self):
-        """
-        Returns the status
-        """
-        # Write your code here
-        return self.status
-        
-# Correct
+            if type(f(key_a, value_a)) == int:
+                result[1].setdefault(key_a, value_a)
+
+    for key, value in d2.items():
+        if type(f(key, value)) == int:
+            result[1].setdefault(key, value)
+
+    inter = {}
+    diff = {}
+
+    for key in d1.keys():
+        if key in d2:
+            inter[key] = f(d1[key], d2[key])
+        else:
+            diff[key] = d1[key]
+
+    for key in d2:
+        if key not in inter:
+            diff[key] = d2[key]
+
+    return inter, diff
